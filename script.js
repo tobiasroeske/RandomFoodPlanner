a7e9b2cf2b3075cd45bd97ff3a83c308c1df2569
Tobias Roeske <t.roeske91@gmail.com>
2023-10-18 16:04:04 +0200

    final chorter version

--- script.js
+++ script.js
@@ -1,43 +1,27 @@
-// First Array with Weekdays:
 
 const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-
-// Second array with breakfast options:
 const breakfast = ['Musli', 'Porridge', 'Zopf', 'Toast with fried egg'];
-
-// Third array with lunch options:
-
-const lunch = ['Sandwich', 'Quinoa salad', 'Lentils salad', 'Empanada'];
-
-// Fourth array with dinner options:
-
+const lunch = ['Sandwiches', 'Quinoa salad', 'Lentils salad', 'Empanada'];
 const dinner = ['Curry', 'Pasta with Pesto', 'Risotto', 'Burker King'];
-
 const batch = [breakfast, lunch, dinner];
-
-console.log(batch[0][1]);
-
-
-
-
-// chooses a random day from the days array
-const chosenDay = () => {
-    let randomDay = days[Math.floor(Math.random() * 7)];
-    return randomDay;
-}
-
-// Creates a random Number to address the index of the lunch and dinner arrays
-
-
 
 // builds a random combination of meal choices for breakfast, lunch, dinner
 const randomCombination = () => {
     let chooseArr = [];
+    let randomDay = days[Math.floor(Math.random() * 7)];
     for (let i = 0; i < 3; i++) {
         let randomNumber = Math.floor(Math.random()) * 4;
         chooseArr.push(batch[i][randomNumber]);
     }
-    return chooseArr;
+    // Message 
+    console.log('Good morning!');
+    console.log(`Today is ${randomDay}`);
+    console.log('We have some meal suggestions for you:');
+    console.log(`Because it is ${randomDay}, we think ${chooseArr[0]}, would best the way to start in the day`);
+    console.log(`Later for your Lunchbreak you could have some ${chooseArr[1]}`);
+    console.log(`And for the evening you should treat yourself with ${chooseArr[2]}`);
 }
 
-// Message 
+randomCombination();
+
+
