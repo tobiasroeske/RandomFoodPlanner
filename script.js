2260d286d8c092a7079f945070f9b28ab23b3919
Tobias Roeske <t.roeske91@gmail.com>
2023-10-18 14:35:48 +0200

    first commid added the data arrays

---
+++ script.js
@@ -0,0 +1,18 @@
+// First Array with Weekdays:
+
+const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+console.log(days);
+
+// Second array with breakfast options:
+const breakfast = ['Musli', 'Porridge', 'Zopf', 'Toast with fried egg'];
+
+// Third array with lunch options:
+
+const lunch = ['Sandwich', 'Quinoa salad', 'Lentils salad', 'Empanada'];
+
+// Fourth array with dinner options:
+
+const dinner = ['Curry', 'Pasta with Pesto', 'Risotto', 'Burker King'];
+
+
+
